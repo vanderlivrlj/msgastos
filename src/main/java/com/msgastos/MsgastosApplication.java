f8474fa301fd1b0cb403a64172a6d6d8d71a88ae
vanderlivrlj <vanderlidev@gmail.com>
2025-04-05 17:55:01 -0300

    Initial commit

---
+++ src/main/java/com/msgastos/MsgastosApplication.java
@@ -0,0 +1,13 @@
+package com.msgastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgastosApplication.class, args);
+	}
+
+}
